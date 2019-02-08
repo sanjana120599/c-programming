15a262228221d39ba5c97c9b32d609659827c7a0
sanjana120599 <47449237+sanjana120599@users.noreply.github.com>
2019-02-08 19:26:39 +0530

    Create positivenegative.c

---
+++ positivenegative.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("enter a number");
+    scanf("%d",&number);
+    if(number>=0)
+    printf("Positive");
+    else
+    printf("Negative");
+return 0;
+}
